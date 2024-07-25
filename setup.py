aead9b53e2bbf20a3215256137f647b63493a483
Steve Holland <sdh4@iastate.edu>
2024-07-25 16:27:42 -0500

    Changes to support automatic building from setuptools

--- setup.py
+++ setup.py
@@ -9,13 +9,42 @@ import re
 import shutil
 from setuptools import setup, Extension
 from setuptools.command.install import install
-import distutils
-from distutils.command.build import build
-from distutils.command.build_ext import build_ext
-from distutils.sysconfig import get_config_var
+#import distutils
+from setuptools import Command
+from setuptools.command.build import build
+from setuptools.command.build_ext import build_ext
+from setuptools.sysconfig import get_config_var
 import subprocess
 import glob
 
+class CMakePreBuild(Command):
+    def initialize_options(self):
+        self.bdist_dir=None
+        pass
+
+    def finalize_options(self):
+        with suppress(Exception):
+            self.bdist_dir=Path(self.get_finalized_command("bdist_wheel").bdist_dir)
+            pass
+        pass
+
+    def run(self):
+        if self.bdist_dir is not None:
+            self.bdist_dir.mkdir(parents=True, exist_ok=True)
+            subprocess.call(["cmake", "-B", self.bdist_dir, "-S", "."])
+            extra_build_params=[]
+            if os.name == "nt":
+                extra_build_params += ["--", "/p:Configuration=RelWithDebInfo"] # Use RelWithDebInfo for visual studio
+                pass
+            subprocess.call(["cmake", "--build", self.bdist_dir, "-j", "8"]+extra_build_params)
+            pass
+        pass
+    pass
+
+class BuildWithPreBuild(build):
+    sub_commands=[("cmake_prebuild", None)]+build.sub_commands
+    pass
+           
 
 class build_ext_from_cmake(build_ext):
     def build_extension(self,ext):
@@ -28,57 +57,26 @@ class build_ext_from_cmake(build_ext):
         pass
     pass
 
-
-# Extract GIT version (use subprocess.call(['git','rev-parse']) to check if we are inside a git repo
-if distutils.spawn.find_executable("git") is not None and subprocess.call(['git','rev-parse'],stderr=subprocess.DEVNULL)==0:
-    # Check if tree has been modified
-    modified = subprocess.call(["git","diff-index","--quiet","HEAD","--"]) != 0
-    
-    gitrev = subprocess.check_output(["git","rev-parse","HEAD"]).decode('utf-8').strip()
-
-    version = "0.0+git-%s" % (gitrev)
-
-    # See if we can get a more meaningful description from "git describe"
-    try:
-        versionraw=subprocess.check_output(["git","describe","--tags","--match=v*"],stderr=subprocess.STDOUT).decode('utf-8').strip()
-        # versionraw is like v0.1.0-50-g434343
-        # for compatibility with PEP 440, change it to
-        # something like 0.1.0+50.g434343
-        matchobj=re.match(r"""v([^.]+[.][^.]+[.][^-.]+)(-.*)?""",versionraw)
-        version=matchobj.group(1)
-        if matchobj.group(2) is not None:
-            version += '+'+matchobj.group(2)[1:].replace("-",".")
-            pass
-        pass
-    except subprocess.CalledProcessError:
-        # Ignore error, falling back to above version string
-        pass
-
-    if modified and version.find('+') >= 0:
-        version += ".modified"
-        pass
-    elif modified:
-        version += "+modified"
-        pass
+# Use presence of a CMakeCache.txt file to determine whether we are in a build directory
+if os.path.exists("CMakeCache.txt"):
+    build_directory = "."
+    build_class = build # If we are in a build directory, just use the regular setuptools build class
     pass
 else:
-    version = "0.0+UNKNOWN"
+    build_directory = "build"
+    build_class = BuildWithPreBuild 
     pass
-
-print("version = %s" % (version))
-
-
 
 python_full_ext_suffix = get_config_var('EXT_SUFFIX') # extension suffix; generally including python version info 
 python_ext_suffix = os.path.splitext("junk."+python_full_ext_suffix)[1] # .so on Linux/MacOS or .pyd on Windows -- we need this because the CMake build doesn't include the python version information in its generated suffix
-# SHLIB_SUFFIX doesn't work on Windows -- distutils.ccompiler.new_compiler().shared_lib_extension is the closest Windows alternative
+# SHLIB_SUFFIX doesn't work on Windows -- setuptools.ccompiler.new_compiler().shared_lib_extension is the closest Windows alternative
 if os.name == 'nt':
-    import distutils.ccompiler
-    platform_shlib_suffix = distutils.ccompiler.new_compiler().shared_lib_extension
+    import setuptools.ccompiler
+    platform_shlib_suffix = setuptools.ccompiler.new_compiler().shared_lib_extension
 else:
     platform_shlib_suffix = get_config_var('SHLIB_SUFFIX')
 
-ext_modules=[Extension("spatialnde2._spatialnde2_python",sources=["spatialnde2/_spatialnde2_python"+python_ext_suffix])] # The "source file" is the cmake-generated binary
+ext_modules=[Extension("spatialnde2._spatialnde2_python",sources=[os.path.join(build_directory,"spatialnde2","_spatialnde2_python"+python_ext_suffix)])] # The "source file" is the cmake-generated binary
 
 spatialnde2_dlls = [ dllname for dllname in os.listdir('spatialnde2') if (dllname.endswith(platform_shlib_suffix) and not dllname.startswith('_')) or dllname.endswith('.lib') ] # Get dlls and .libs but not the extension itself -- which has a name that starts with an underscore
 
@@ -97,13 +95,16 @@ package_data = {
 setup(name="spatialnde2",
       description="spatialnde2",
       author="Stephen D. Holland",
-      version=version,
       url="http://thermal.cnde.iastate.edu/spatialnde2.xhtml",
       ext_modules = ext_modules,
       zip_safe = False,
       packages=["spatialnde2"],
+      package_dir={ "spatialnde2": build_directory }
       package_data=package_data,
-      cmdclass = { "build_ext": build_ext_from_cmake } )
+      cmdclass = {
+          "build": build_class, # Use our custom BuildWithPreBuild if we are not already in a build directory
+          "cmake_prebuild": CMakePreBuild,
+          "build_ext": build_ext_from_cmake } )
 
 
             
